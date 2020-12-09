aa9bbdb7ed11523a3dabca5f75e94bc5990b03a2
Chase Simmons <csim6890@gmail.com>
2020-12-08 23:34:15 -0600

    wip cycler for slides

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,32 @@ import './Header.css';
 
 // -> IMPORT COMPONENT <-- \\
 const logo = require('./Header_images/logo.png');
+const slideshowItem = require('./Header_images/slideshow_1.jpg');
 // -> IMPORT COMPONENT <-- \\
 
 class Header extends Component {
+  state = {
+    slideNum: 1,
+  };
+
+  componentDidMount() {
+    this.updateSlide();
+  }
+
+  updateSlide = () => {
+    setTimeout(() => {
+      console.log('working');
+      this.updateSlide();
+    }, 5000);
+  };
   render() {
     return (
       <header>
-        <div className="header-video"></div>
+        <div className="header-banner">
+          <div>
+            <img src={slideshowItem} />
+          </div>
+        </div>
         <div className="header-logo">
           <img src={logo} alt="Wizardry-Online"></img>
         </div>
