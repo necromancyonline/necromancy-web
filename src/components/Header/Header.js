0a303fec376f25bd3bdf25140359ed5f04054e77
Chase Simmons <csim6890@gmail.com>
2020-12-09 01:33:09 -0600

    slideshow functionality impletemented needs more pictures >.>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,40 +5,89 @@ import './Header.css';
 // -> IMPORT COMPONENT <-- \\
 const logo = require('./Header_images/logo.png');
 let slideshowItem = require('./Header_images/slideshow_1.jpg');
+let nextSlideshowItem = require('./Header_images/slideshow_2.jpg');
 
 // -> IMPORT COMPONENT <-- \\
-
+let opacityHandler = 1;
+let transitionHandler = 2;
 let slideNum = 1;
+let nextSlideNum = 2;
 class Header extends Component {
-  state = {};
+  state = {
+    hasLoaded: false,
+  };
 
   reRender = () => {
     this.setState({});
   };
   componentDidMount() {
+    this.handleFade();
     this.updateSlide();
+    this.setState({
+      hasLoaded: true,
+    });
   }
 
+  handleFade = () => {
+    setInterval(() => {
+      opacityHandler = 1;
+      transitionHandler = 0;
+      this.reRender();
+    }, 10000);
+
+    const opacityTimer = () => {
+      let interval;
+      if (this.state.hasLoaded === true) {
+        interval = 10000;
+      } else {
+        interval = 8000;
+      }
+      setTimeout(() => {
+        opacityHandler = 0;
+        transitionHandler = 2;
+        this.reRender();
+        opacityTimer();
+      }, interval);
+    };
+    opacityTimer();
+  };
   updateSlide = () => {
-    setTimeout(() => {
+    setInterval(() => {
       if (slideNum === 1) {
         slideNum++;
         slideshowItem = require(`./Header_images/slideshow_${slideNum}.jpg`);
-        this.reRender();
+        nextSlideNum--;
+        nextSlideshowItem = require(`./Header_images/slideshow_${nextSlideNum}.jpg`);
       } else {
         slideNum--;
         slideshowItem = require(`./Header_images/slideshow_${slideNum}.jpg`);
-        this.reRender();
+        nextSlideNum++;
+        nextSlideshowItem = require(`./Header_images/slideshow_${nextSlideNum}.jpg`);
       }
-      this.updateSlide();
+      this.reRender();
+      console.log('slide');
     }, 10000);
   };
+
   render() {
+    console.log(slideshowItem);
     return (
       <header>
-        <div className="header-banner">
+        <div
+          className="header-banner"
+          style={{
+            backgroundImage: `url( ${nextSlideshowItem})`,
+          }}
+        >
           <div>
-            <img className="banner-image" src={slideshowItem} />
+            <img
+              className="banner-image"
+              src={slideshowItem}
+              style={{
+                opacity: opacityHandler,
+                transition: `${transitionHandler}s`,
+              }}
+            />
           </div>
         </div>
         <div className="header-logo">
@@ -49,7 +98,7 @@ class Header extends Component {
           <nav className="header-nav">
             <div className="header-block"></div>
           </nav>
-        </div>
+        </div>{' '}
       </header>
     );
   }
