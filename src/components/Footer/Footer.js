1c64136151f07ccb21b31068aa1b9f495fc1a1da
Chase Simmons <csim6890@gmail.com>
2020-12-11 17:22:41 -0600

    footer styling moved links to foooter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
+import Patreon from '../Patreon/Patreon';
 import './Footer.css';
 
+const github = require('./Footer_images/GitHubLogo.png');
+const patreon = require('./Footer_images/PatreonLogo.png');
+
 class Footer extends Component {
-  componentDidMount() {}
+  onClickLink = (link) => (event) => {
+    switch (link) {
+      case 'github':
+        window.open('https://github.com/necromancyonline');
+        break;
+      case 'patreon':
+        window.open(
+          'https://www.patreon.com/NecromancyOnline?fbclid=IwAR0_2ZOeBqtsaWrYaTJt2KiByjKJ4cBr5JYSgVoQ9oFu87QVQtvgYFmu2tc'
+        );
+        break;
+      default:
+        return;
+    }
+  };
 
   render() {
     return (
       <footer className="footer-box">
-        {/* <div className="header-blur" /> */}
-        <div className="App-footer"></div>
+        <div className="App-footer">
+          <div className="footer-links">
+            <div className="footer-link-item">
+              <img
+                className="footer-link-img"
+                src={github}
+                onClick={this.onClickLink('github')}
+              />
+            </div>
+            <div className="footer-link-item">
+              <img
+                className="footer-link-img"
+                src={patreon}
+                onClick={this.onClickLink('patreon')}
+              />
+            </div>
+          </div>
+        </div>
       </footer>
     );
   }
